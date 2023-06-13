ae4abd8c99d8ef70b9a7588d228fbe4cace6f77d
QuentinPrst <quentinprost13@gmail.com>
2023-06-13 17:43:49 +0200

    First API comp commit

--- Source/Comp.h
+++ Source/Comp.h
@@ -7,13 +7,49 @@
 
 #pragma once
 
+#include "JuceHeader.h"
+#include "CompAhr.h"
+
+using EstimationType = juce::dsp::BallisticsFilterLevelCalculationType;
+
 template <typename SampleType>
+struct CompParams {
+    SampleType attack;
+    SampleType hold;
+    SampleType release;
+    SampleType threshold;
+    SampleType ratio;
+    SampleType knee;
+    SampleType makeUpGain;
+    SampleType dryWetMix;
+    SampleType lookaheadTime;
+    EstimationType estimationType;
+};
 
 template <typename SampleType>
 class Comp {
 public:
+    Comp();
+    ~Comp();
     
+    void prepare(const juce::dsp::ProcessSpec& spec);
+    void setAttack(SampleType attack);
+    void setHold(SampleType hold);
+    void setRelease(SampleType release);
+    void setThreshold(SampleType threshold);
+    void setRatio(SampleType ratio);
+    void setKnee(SampleType knee);
+    void setBypass(bool active);
+    void setLookAhead(bool active, float lookAheadTime);
+    void setEstimationType(EstimationType type);
     
+    void processBlock(const juce::dsp::ProcessContextNonReplacing<SampleType>& context);
+    SampleType processSample(SampleType input);
 public:
-    <#member functions#>
+    juce::AudioBuffer<SampleType> mControlGain;
+    juce::AudioBuffer<SampleType> mSideChain;
+    CompParams<SampleType> mParams;
+    CompAhr<SampleType> ahr;
+    juce::dsp::BallisticsFilter<SampleType> ballistic;
+    
 };
