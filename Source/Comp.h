b3013eef1af741366b4b542b3a94018118c956c9
QuentinPrst <quentinprost13@gmail.com>
2023-06-13 17:01:43 +0200

    Added compressor module

---
+++ Source/Comp.h
@@ -0,0 +1,19 @@
+/*
+  ==============================================================================
+    Comp.h
+    Author:  Quentin Prost
+  ==============================================================================
+*/
+
+#pragma once
+
+template <typename SampleType>
+
+template <typename SampleType>
+class Comp {
+public:
+    
+    
+public:
+    <#member functions#>
+};
